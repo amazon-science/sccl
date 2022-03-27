a79345996138c6791a1e30feaeb0b7f7f4130a66
PiDeSense <33806048+scilearner@users.noreply.github.com>
2022-03-27 19:42:35 -0400

    list all datasets

--- preprocess_data.sh
+++ preprocess_data.sh
@@ -9,3 +9,13 @@ do
     python AugData/nlpaug_explore.py --dataset $name --gpu_id 1 &
     sleep 10
 done
+
+# python AugData/nlpaug_explore.py --dataset agnews --gpu_id 0
+# python AugData/nlpaug_explore.py --dataset biomedical --gpu_id 1
+# python AugData/nlpaug_explore.py --dataset googlenews_S --gpu_id 1
+# python AugData/nlpaug_explore.py --dataset googlenews_TS --gpu_id 1
+# python AugData/nlpaug_explore.py --dataset googlenews_T --gpu_id 1
+# python AugData/nlpaug_explore.py --dataset search_snippets --gpu_id 1
+# python AugData/nlpaug_explore.py --dataset biomedical --gpu_id 1
+# python AugData/nlpaug_explore.py --dataset tweet --gpu_id 1
+# 
